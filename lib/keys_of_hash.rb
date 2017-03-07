f21c48620240aea8b081a71e4024c1d61745e8c4
Meryl Dakin <meryl.k.dakin@gmail.com>
2017-03-07 22:55:56 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    # binding.pry
+    keys_array = []
+    arguments.each do |country_array|
+      self.each do |animal, country_value|
+        if country_array == country_value
+          keys_array.push(animal)
+        end
+      end
+    end
+    return keys_array
   end
 end
